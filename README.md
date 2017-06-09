884ef4240a97c5331275522d234b04f24513a9e0
Manuel83 <manuelfritsch@Manuels-MBP.fritz.box>
2017-06-09 22:11:03 +0200

    First Version

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # alexa
 Amazon Alexa Plugin for CraftBeerPi
+
+# Installation
+
+Please download and install this plugin via the CraftBeerPi user interface.
+
+You need to run the following command on your Raspberry Pi to install the missing python libs
+
+<code>pip install flask-ask</cod>
+
+## Flask Ask Video Tutorial. Just follow this tutorial and just use the CBPi IntentSchema and Utterances
+https://www.youtube.com/watch?v=cXL8FDUag-s
+
+## Ngrok
+
+Install ngrok to create a secure tunnel to your local network.
+
+https://ngrok.com/
+
+###
+Intent Schema
+
+<code>
+{
+  "intents": [
+    {
+      "intent": "Goodbye"
+    },
+    {
+      "intent": "Update"
+    },
+    {
+      "intent": "StepIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "Sensor",
+          "type": "AMAZON.LITERAL"
+        }
+      ],
+      "intent": "SensorIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "K",
+          "type": "AMAZON.LITERAL"
+        }
+      ],
+      "intent": "KettleIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "Actor",
+          "type": "AMAZON.LITERAL"
+        }
+      ],
+      "intent": "ActorIntent"
+    }
+  ]
+}
+</code>
+
+### Sample Utterances
+
+<code>
+
+
+Goodbye No thanks
+Goodbye Stop
+Update for the current state
+Update give my a the current status
+Update current status
+Update status update please
+StepIntent what step is running
+StepIntent what is the current step
+StepIntent current step
+SensorIntent  value of sensor {Test|Sensor}
+SensorIntent  value of sensor {Sensor|Sensor}
+SensorIntent  value of sensor {Thermometer|Sensor}
+SensorIntent  what is value of sensor {Test|Sensor}
+SensorIntent  what is current value of sensor {Test|Sensor}
+SensorIntent  what is the current value of sensor {Test|Sensor}
+SensorIntent tell me the value of sensor {Test|Sensor}
+KettleIntent  temperature of kettle {Mash Tun|K}
+KettleIntent  what is temperature of kettle {Mash Tun|K}
+KettleIntent  what is current temperature of kettle {Mash Tun|K}
+KettleIntent  what is the current temperature of kettle {Mash Tun|K}
+KettleIntent  temperature of kettle {Hot Liquor Tank|K}
+KettleIntent  what is temperature of kettle {Hot Liquor Tank|K}
+KettleIntent  what is current temperature of kettle {Hot Liquor Tank|K}
+KettleIntent  what is the current temperature of kettle {Hot Liquor Tank|K}
+KettleIntent  temperature of kettle {Boil Kettle|K}
+KettleIntent  what is temperature of kettle {Boil Kettle|K}
+KettleIntent  what is current temperature of kettle {Boil Kettle|K}
+KettleIntent  what is the current temperature of kettle {Boil Kettle|K}
+ActorIntent  toggle actor {heater|Actor}
+ActorIntent  toggle actor {agiator|Actor}
+ActorIntent  toggle actor {pump|Actor}
+</code>
---
+++ __init__.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from flask_ask import Ask, request, session, question, statement
+
+from modules import app, cbpi
+import logging
+
+
+ask = Ask(app, "/")
+
+@ask.launch
+def launch():
+    speech_text = 'Welcome to CraftBeerPi Alexa Plugin'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.intent('Update')
+def update():
+    s = cbpi.cache.get("active_step")
+    text = "Here is your update. "
+    if s is not None:
+        text = text + 'The current step is %s . ' % (s.name,)
+
+    else:
+        text = text + 'There is no brewing process running .'
+    text = text + "Now to the brewing kettles. "
+
+    for idx, value in cbpi.cache["kettle"].iteritems():
+        current_sensor_value = cbpi.get_sensor_value(value.sensor)
+        text = text + "%s has set target temperature to %s and a current temperature of %s. " % (value.name, value.target_temp, current_sensor_value)
+
+    text = text + "Any further question?"
+
+    return question(text).reprompt(text).simple_card('CBP Update', text)
+
+@ask.intent('SensorIntent', mapping={'sensor': 'Sensor'})
+def sensor_value(sensor):
+    print "SENSOR", sensor
+    for idx, value in cbpi.cache["sensors"].iteritems():
+        print value.__dict__
+        if value.name.lower() == sensor:
+            v = cbpi.get_sensor_value(value.id)
+            speech_text = 'The sensor temperature is %s %s' % (v, value.instance.get_unit())
+            return question(speech_text).reprompt(speech_text).simple_card('Value Name', speech_text)
+
+    speech_text = "I have notf found the sensor %s. Any further question?" % sensor
+    return question(speech_text).reprompt(speech_text).simple_card('Sensor Value', speech_text)
+
+@ask.intent('KettleIntent', mapping={'kettle': 'K'})
+def kettle_value(kettle):
+    print "KETTLE", kettle
+    for idx, value in cbpi.cache["kettle"].iteritems():
+        if value.name.lower() == kettle:
+            v = cbpi.get_sensor_value(value.sensor)
+            s = cbpi.cache["sensors"].get(int(value.sensor))
+            speech_text = 'The Kettle Temperature of %s is %s %s' % (str(kettle), v, s.instance.get_unit())
+
+            return question(speech_text).reprompt(speech_text).simple_card('Kettle Value', speech_text)
+            #return statement(speech_text).simple_card('Kettle Value', speech_text)
+
+        speech_text = "I have not found the kettle %s. Any further question?" % kettle
+    return question(speech_text).reprompt(speech_text).simple_card('Kettle Value', speech_text)
+
+
+@ask.intent('ActorIntent', mapping={'actor': 'Actor'})
+def toggle_actor(actor):
+
+    for idx, value in cbpi.cache["actors"].iteritems():
+        if value.name.lower() == actor:
+
+            if value.state == 1:
+                cbpi.switch_actor_off(value.id)
+                speech_text = 'Ok, I switched %s off ' % actor
+            else:
+                cbpi.switch_actor_on(value.id)
+                speech_text = 'Ok, I switched %s on ' % actor
+
+            return question(speech_text).reprompt(speech_text).simple_card('Actor Toggle', speech_text)
+
+        speech_text = "I have not found the actor %s. Any further question?" % actor
+    return question(speech_text).reprompt(speech_text).simple_card('Kettle Value', speech_text)
+
+
+@ask.intent('StepIntent')
+def step_intent(kettle):
+    s = cbpi.cache.get("active_step")
+    if s is not None:
+        speech_text = 'The current step is %s' % (s.name, )
+        return question(speech_text).reprompt(speech_text).simple_card('Step Name', speech_text)
+
+    else:
+        speech_text = 'There is no brewing process running'
+        return question(speech_text).reprompt(speech_text).simple_card('Step Name', speech_text)
+
+@ask.intent('Goodbye')
+def goodbye(kettle):
+    speech_text = "OK don't for get to donate some beer to Manuel! Goodbye"
+    return statement(speech_text).simple_card("Goodbye", speech_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
